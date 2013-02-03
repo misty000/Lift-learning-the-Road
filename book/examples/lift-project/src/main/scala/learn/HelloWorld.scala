426a486ecee5896b0e412ce612cba5ddb0eab9e7
Yang Jing <yangbajing@gmail.com>
2013-02-03 09:50:03 +0800

    更新sbt-launch到0.12.2

--- book/examples/lift-project/src/main/scala/learn/HelloWorld.scala
+++
@@ -1,9 +0,0 @@
-package learn
-
-object HelloWorld {
-  def main(args: Array[String]) {
-    val msg = "您好" :: "羊八井" :: Nil mkString ("", "，", "！")
-    println(msg)
-  }
-}
-
--- sbt.bat
+++ sbt.bat
@@ -1,2 +1,2 @@
 set SCRIPT_DIR=%~dp0
-java  -Xms512M -Xmx1024M -Xss1M -XX:MaxPermSize=256M -Dfile.encoding=UTF-8 -XX:+CMSClassUnloadingEnabled -jar "%SCRIPT_DIR%\project\sbt-launch-0.12.1.jar" %*
+java  -Xms512M -Xmx1024M -Xss1M -XX:MaxPermSize=256M -Dfile.encoding=UTF-8 -XX:+CMSClassUnloadingEnabled -jar "%SCRIPT_DIR%\project\sbt-launch-0.12.2.jar" %*
