c15786d3331e2ff008b6cd9773db4918c46fef38
Yang Jing <yangbajing@gmail.com>
2013-02-10 23:08:54 +0800

    删除目录

--- core/common/src/main/scala/yangbajing/common/Datas.scala
+++
@@ -1,9 +0,0 @@
-package yangbajing.common
-
-case class NV[N, V](n: N, v: V)
-
-case class KV[K, V](k: K, v: V)
-
-case class XY[X, Y](x: X, y: Y)
-
-case class XYZ[X, Y, Z](x: X, y: Y, z: Z)
--- core/persistence/src/main/scala/yangbajing/persistence/data/MetaList.scala
+++
@@ -1,5 +0,0 @@
-package yangbajing.persistence.data
-
-import scala.collection.mutable
-
-case class MetaList[V](var data: List[V])
